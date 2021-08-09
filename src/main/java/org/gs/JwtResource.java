a7cea72d93e3b3c37b91e355b2387a5bc35e4ee5
Giuseppe Scaramuzzino <giuse.scara@gmail.com>
2021-08-09 16:41:27 +0200

    Quarkus Insight MP specs

--- src/main/java/org/gs/JwtResource.java
+++ src/main/java/org/gs/JwtResource.java
@@ -1,10 +1,13 @@
 package org.gs;
 
+import io.smallrye.jwt.build.Jwt;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Date;
 
 @Path("/jwt")
 public class JwtResource {
@@ -12,7 +15,13 @@ public class JwtResource {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public Response getToken() {
-    String token = "";
+    String token =
+        Jwt.claims()
+            .issuedAt(new Date().toInstant())
+            .subject("giuseppe")
+            .groups("Admin")
+            .claim("demo", "quarkus")
+            .sign();
     return Response.ok(token).build();
   }
 }
